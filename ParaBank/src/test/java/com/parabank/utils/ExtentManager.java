690516fa234fdd59b19cb8d25cf6a803a2cf207d
hemu0119 <2100080119ai.ds@gmail.com>
2026-06-15 10:20:15 +0530

    Update ExtentManager.java

--- ParaBank/src/test/java/com/parabank/utils/ExtentManager.java
+++ ParaBank/src/test/java/com/parabank/utils/ExtentManager.java
@@ -2,7 +2,6 @@ package com.parabank.utils;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentManager {
 
@@ -12,30 +11,20 @@ public class ExtentManager {
 
         if (extent == null) {
 
-            // Create Spark Reporter
             ExtentSparkReporter spark =
                     new ExtentSparkReporter(
                             "test-output/ExtentReport.html");
 
-            // Report Configuration
             spark.config().setReportName(
                     "ParaBank Automation Report");
 
             spark.config().setDocumentTitle(
                     "ParaBank Extent Report");
 
-            spark.config().setTheme(
-                    Theme.STANDARD);
-
-            spark.config().setTimeStampFormat(
-                    "dd.MM.yyyy hh:mm:ss a");
-
-            // Create Extent Report
             extent = new ExtentReports();
 
             extent.attachReporter(spark);
 
-            // System Information
             extent.setSystemInfo(
                     "Tester", "Hemanth Kumar");
 
@@ -44,14 +33,6 @@ public class ExtentManager {
 
             extent.setSystemInfo(
                     "Browser", "Chrome");
-
-            extent.setSystemInfo(
-                    "Operating System",
-                    System.getProperty("os.name"));
-
-            extent.setSystemInfo(
-                    "Java Version",
-                    System.getProperty("java.version"));
         }
 
         return extent;
